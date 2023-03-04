a74d102376d735f4a7be61d35786ea3ef3b2c97e
octav20 <octav20>
2023-03-03 22:59:57 -0700

    correcion ssl

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ export const pool = createPool({
   host: DB_HOST,
   port: DB_PORT,
   database: DB_NAME,
-  ssl: {
-    rejectUnauthorized: true
-  }
+  // ssl: {
+  //   rejectUnauthorized: false
+  // }
 });
